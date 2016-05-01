ec9155b0cc380a122aa9a81012b07d4f54062095
Adam Slater <adam.slater.1010@gmail.com>
2016-05-01 12:00:48 -0700

    added breakout screen

--- js/breakout.js
+++ js/breakout.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
+var game = new Phaser.Game(screen.availWidth, screen.availHeight * 0.85, Phaser.AUTO, null, {
     preload: preload, create: create, update: update
 });
 function preload() {}
