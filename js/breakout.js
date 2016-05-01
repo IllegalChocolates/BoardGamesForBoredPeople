0efe70b4862a987be523a6594396751c328744c5
Adam Slater <adam.slater.1010@gmail.com>
2016-05-01 12:25:39 -0700

    added basic ball and paddle functionality

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,40 @@
+var ball;
+var paddle;
+
 var game = new Phaser.Game(screen.availWidth, screen.availHeight * 0.85, Phaser.AUTO, null, {
     preload: preload, create: create, update: update
 });
-function preload() {}
-function create() {}
-function update() {}
+function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+
+    game.stage.backgroundColor = '#eee';
+
+    game.load.image('ball',   '../images/breakout/ball.png');
+    game.load.image('paddle', '../images/breakout/paddle.png');
+
+
+}
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    ball = game.add.sprite(game.world.width*0.5, game.world.height-25, 'ball');
+    ball.anchor.set(0.5);
+    game.physics.enable(ball, Phaser.Physics.ARCADE);
+    ball.body.velocity.set(150, -150);
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.set(1);
+
+    paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+    paddle.anchor.set(0.5,1);
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+    paddle.body.immovable = true;
+
+
+}
+function update() {
+    game.physics.arcade.collide(ball, paddle);
+    paddle.x = game.input.x || game.world.width*0.5;
+
+}
